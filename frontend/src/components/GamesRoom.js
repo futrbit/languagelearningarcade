b59dd4c6b7741a825ee4c7a9cfc6cace689f0e9d
futrbit <futrbit@gmail.com>
2025-07-15 20:41:02 +0100

    Update GamesRoom.js

--- frontend/src/components/GamesRoom.js
+++ frontend/src/components/GamesRoom.js
@@ -1,4 +1,3 @@
-// src/components/GamesRoom.js
 import React from "react";
 import WordChainGame from "./WordChainGame";
 import QuizGame from "./QuizGame";
@@ -6,11 +5,15 @@ import GuessTheWord from "./GuessTheWord";
 import MemoryMatch from "./MemoryMatch";
 import FastGrammarRace from "./FastGrammarRace";
 import DialogueFillIn from "./DialogueFillIn";
+import WordSearchGame from "./WordSearchGame";
+import CrosswordGame from "./CrosswordGame";
 import MillionaireGame from "./MillionaireGame";
 import BlanketyBlank from "./BlanketyBlank";
 import Blockbusters from "./Blockbusters";
 import BlindDate from "./BlindDate";
 import WordChainGameExpanded from "./WordChainGameExpanded";
+import CreativeWriting from "./CreativeWriting";
+import VocabularyCards from "./VocabularyCards";
 
 const GamesRoom = () => {
   return (
@@ -23,7 +26,6 @@ const GamesRoom = () => {
           <h3>Word Chain</h3>
           <WordChainGame words={["cat", "tiger", "rabbit", "tarantula", "ant", "tortoise", "elephant", "tapir"]} />
         </div>
-
         <div className="game-card">
           <h3>Quiz Game</h3>
           <QuizGame
@@ -41,50 +43,57 @@ const GamesRoom = () => {
             ]}
           />
         </div>
-
         <div className="game-card">
           <h3>Guess The Word</h3>
           <GuessTheWord />
         </div>
-
         <div className="game-card">
           <h3>Memory Match</h3>
           <MemoryMatch />
         </div>
-
         <div className="game-card">
           <h3>Fast Grammar Race</h3>
           <FastGrammarRace />
         </div>
-
         <div className="game-card">
           <h3>Dialogue Fill-In</h3>
           <DialogueFillIn />
         </div>
-
         <div className="game-card">
-          <h3>Who Wants to Be a Millionaire</h3>
+          <h3>Word Search</h3>
+          <WordSearchGame />
+        </div>
+        <div className="game-card">
+          <h3>Crossword</h3>
+          <CrosswordGame />
+        </div>
+        <div className="game-card">
+          <h3>Millionaire</h3>
           <MillionaireGame />
         </div>
-
         <div className="game-card">
           <h3>Blankety Blank</h3>
           <BlanketyBlank />
         </div>
-
         <div className="game-card">
           <h3>Blockbusters</h3>
           <Blockbusters />
         </div>
-
         <div className="game-card">
           <h3>Blind Date</h3>
           <BlindDate />
         </div>
-
         <div className="game-card">
-          <h3>Word Chain (Expanded)</h3>
+          <h3>Word Chain Expanded</h3>
           <WordChainGameExpanded />
+        </div>
+        <div className="game-card">
+          <h3>Creative Writing</h3>
+          <CreativeWriting />
+        </div>
+        <div className="game-card">
+          <h3>Vocabulary Cards</h3>
+          <VocabularyCards />
         </div>
       </div>
     </div>

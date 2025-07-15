bbcab6832915cfefd78ef980097e4872110a0397
futrbit <futrbit@gmail.com>
2025-07-15 23:55:15 +0100

    Update ClassGenerator.js

--- frontend/src/components/ClassGenerator.js
+++ frontend/src/components/ClassGenerator.js
@@ -712,7 +712,7 @@ export default function ClassGenerator() {
   return (
     <div>
       <header className="header">
-        <img src="/logo.png" alt="Language Learning Arcade Logo" className="header-logo" />
+        
         <nav>
           <a href="/">Home</a>
           <a href="/about">About</a>
